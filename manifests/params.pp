6901e934df0d358a254ef02482f398560514e89d
gabloe <t-gabloe@microsoft.com>
2014-06-26 13:18:20 -0700

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,18 +1,18 @@
 class quartermaster::params {
-  $arp_type = '01',
-  $host_macaddress = regsubst($macaddress, '(\:)','-','G'),
-  $default_pxeboot_option = 'menu.c32',
-  $tmp               = '/tmp',
-  $port              = 80,
-  $logroot           = '/var/log/quartermaster',
-  $tftpboot          = '/srv/tftpboot',
-  $wwwroot           = '/srv/install',
-  $nfsroot           = '/srv/nfs',
-  $bin               = "${wwwroot}/bin",
-  $puppetmaster_fqdn = "${fqdn}",
-  $exe_mode          = '0777',
-  $file_mode         = '0644',
-  $dir_mode          = '0755',
-  $counter           = '0',
-  $nameserver        = '4.2.2.2',
+  $arp_type = '01'
+  $host_macaddress = regsubst($macaddress, '(\:)','-','G')
+  $default_pxeboot_option = 'menu.c32'
+  $tmp               = '/tmp'
+  $port              = 80
+  $logroot           = '/var/log/quartermaster'
+  $tftpboot          = '/srv/tftpboot'
+  $wwwroot           = '/srv/install'
+  $nfsroot           = '/srv/nfs'
+  $bin               = "${wwwroot}/bin"
+  $puppetmaster_fqdn = "${fqdn}"
+  $exe_mode          = '0777'
+  $file_mode         = '0644'
+  $dir_mode          = '0755'
+  $counter           = '0'
+  $nameserver        = '4.2.2.2'
 }
