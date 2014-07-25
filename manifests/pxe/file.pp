33fa4afed50139a770111fe1d0313be89d89a6b8
gabloe <t-gabloe@microsoft.com>
2014-07-25 12:25:00 -0700

    Update file.pp

--- manifests/pxe/file.pp
+++ manifests/pxe/file.pp
@@ -3,13 +3,11 @@ define quartermaster::pxe::file (
   $host_macaddress        = $quartermaster::params::host_macaddress,
   $default_pxeboot_option = $quartermaster::params::default_pxe_option
 ) {
-  file { "${quartermaster::tftpboot}/pxelinux/pxelinux.cfg/${arp_type}-${host_macaddress}":
+  tftp::file { "/pxelinux/pxelinux.cfg/${arp_type}-${host_macaddress}":
     ensure  => directory,
     owner   => 'nobody',
     group   => 'nogroup',
-    mode    => $quartermaster::dir_mode,
     content => template('quartermaster/pxefile.erb')
-    #require => [ Package[ 'tftpd-hpa' ],File[ tftpd_config ]],
   }
 
 }
